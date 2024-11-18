8e3566ab2ae5993847834957f6498f4963a3f241
joao-gallo <contato@kaueguedes.com.br>
2024-11-18 18:45:43 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-
-// next.config.ts
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Desabilita ESLint durante o build
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/v1/:path*', // Rota no frontend
+        destination: 'https://api.blogsdf.uk/v1/:path*', // URL do backend
+      },
+    ];
   },
 };
 
